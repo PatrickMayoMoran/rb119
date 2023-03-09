8aabcbda951e5e9c76be7f0a8073a46df314e40a
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-03-09 22:07:35 +0000

    Add frame of solution for modifying original string

--- other_practice_problems/weird_case.rb
+++ other_practice_problems/weird_case.rb
@@ -18,26 +18,60 @@
 #   join characters back into word
 # # Examples:
 #
+#def to_weird_case(string)
+#  weird_words = string.split.map.with_index do |word, i|
+#    (i + 1) % 3 == 0 ? weird_case(word) : word
+#  end
+
+#  weird_words.join(' ')
+#end
+
+#def weird_case(word)
+#  word.chars.map.with_index { |c,i| i.odd? ? c.upcase : c }.join
+#end
+
+# ALGORITHM
+# MUST alter the original string
+# iterate through characters of the string with index
+# if it's the third word, then change every second letter
+#   if word_index is divisible by 3, then change every second letter
+# 
+# counter for word
+# starts at 1, add 1 when we encounter a space
+#
+# counter for letters 
+#   starts at 1
+#   plus 1 on each iteration
+#   if we hit a space, reset to 1
+
 def to_weird_case(string)
-  weird_words = string.split.map.with_index do |word, i|
-    (i + 1) % 3 == 0 ? weird_case(word) : word
+  word_i = 1
+  letter_i = 1
+  string.each_char.with_index do |c,i|
+    if c == ' '
+      word_i += 1
+      letter_i = 1
+      next
+    elsif word_i % 3 == 0
+      p letter_i
+      p string[0..i]
+      letter_i += 1
+    else
+      next
+    end
   end
 
-  weird_words.join(' ')
+  string
 end
 
-def weird_case(word)
-  word.chars.map.with_index { |c,i| i.odd? ? c.upcase : c }.join
-end
-
-p to_weird_case('Lorem Ipsum is simply dummy text of the printing') ==
- 'Lorem Ipsum iS simply dummy tExT of the pRiNtInG'
- p to_weird_case(
- 'It is a long established fact that a reader will be distracted') ==
- 'It is a long established fAcT that a rEaDeR will be dIsTrAcTeD'
- p to_weird_case('aaA bB c') == 'aaA bB c'
- p to_weird_case(
- 'Miss Mary Poppins word is supercalifragilisticexpialidocious') ==
- 'Miss Mary POpPiNs word is sUpErCaLiFrAgIlIsTiCeXpIaLiDoCiOuS'
+p to_weird_case('Lorem Ipsum is simply dummy text of the printing') #==
+# 'Lorem Ipsum iS simply dummy tExT of the pRiNtInG'
+#p to_weird_case(
+#'It is a long established fact that a reader will be distracted') #==
+# 'It is a long established fAcT that a rEaDeR will be dIsTrAcTeD'
+#p to_weird_case('aaA bB c') #== 'aaA bB c'
+#p to_weird_case(
+# 'Miss Mary Poppins word is supercalifragilisticexpialidocious') #==
+# 'Miss Mary POpPiNs word is sUpErCaLiFrAgIlIsTiCeXpIaLiDoCiOuS'
 #
 # # The tests above should print "true".
