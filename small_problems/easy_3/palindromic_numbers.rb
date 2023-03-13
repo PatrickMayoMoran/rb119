9260c5c57e7bf6fb6fef7ae3cd9f0bb170689fe8
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-03-13 20:11:23 +0000

    Add solutions to number palindromes and upcase problem

---
+++ small_problems/easy_3/palindromic_numbers.rb
@@ -0,0 +1,9 @@
+def palindromic_number?(number)
+  number.digits == number.digits.reverse
+end
+
+p palindromic_number?(34543) == true
+p palindromic_number?(123210) == false
+p palindromic_number?(22) == true
+p palindromic_number?(5) == true
+p palindromic_number?(0034300) == true
