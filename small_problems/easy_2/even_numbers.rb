97f4a04fc7e43da98cd636327043d88ebf323a8a
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-03-13 14:25:15 +0000

    Add provided solution and other user solution to even numbers

--- small_problems/easy_2/even_numbers.rb
+++ small_problems/easy_2/even_numbers.rb
@@ -7,4 +7,15 @@
 # (1..99).each { |n| puts n if n.even? }
 #
 # OPTION 3: step and substraction
-1.step(99,2) { |i| puts (i - 1) unless i == 1 }
+# 1.step(99,2) { |i| puts (i - 1) unless i == 1 }
+
+# OPTION 4: explicit while loop
+num = 1
+while num <= 99
+  puts num if num.even?
+  num += 1
+end
+
+# OPTION 5: other user submitted solution, only iterates over needed numbers
+# by calling select on range
+puts (1..99).select(&:even?)
