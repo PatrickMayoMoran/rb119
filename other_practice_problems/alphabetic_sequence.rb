202c9d182603d7f55621fd48582c308abdfbe0af
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-03-17 21:00:17 +0000

    Add solutions to odd_not_prime practice problem and alphabetic sequence problem

--- other_practice_problems/alphabetic_sequence.rb
+++ other_practice_problems/alphabetic_sequence.rb
@@ -72,3 +72,86 @@ p longest('abcdeapbcdef') #==  'abcde'
 # p longest('asdfbyfgiklag') == 'fgikl'
 # p longest('z') == 'z'
 # p longest('zyba') == 'z'
+
+
+def is_alphabetic?(string)
+  string.chars.sort == string.chars
+end
+
+def get_substrings(string)
+  substrings = []
+  (0...string.size).each do |i|
+    (i...string.size).each do |ii|
+      slice = string[i..ii]
+      substrings << slice
+    end
+  end
+
+  substrings
+end
+
+def longest(string)
+  substrings = get_substrings(string)
+  alphabetic_subs = substrings.select { |s| is_alphabetic?(s)}
+  alphabetic_subs.max_by { |s| s.size }
+end
+
+p longest('asd') == 'as'
+p longest('nab') == 'ab'
+p longest('abcdeapbcdef') ==  'abcde'
+p longest('asdfaaaabbbbcttavvfffffdf') == 'aaaabbbbctt'
+p longest('asdfbyfgiklag') == 'fgikl'
+p longest('z') == 'z'
+p longest('zyba') == 'z'
+
+=begin
+# input: string
+  # output:string
+    # where string is the longest alpabetical substring from input
+
+# Observations:
+  # if two substrings match, return the earlier one in the string
+#
+
+# Logic:
+  # create a container for alphabetical substrings: []
+  # create selector
+  # iterate through the string with index
+    # if current char.ord is greater than next char.ord store: alphabetical_substr << string[selector..current_index]
+    # reassign selector to current_index + 1
+    # proceed to next char
+  # 
+  # To select largest
+    # get max size array.map { |e| e.size }.max => store return in variable max
+    # select alphabetical substrings of that size: array.select { |e| e.size == max }[0] (RETURNS FIRST OCCURANCE IF MULTIPLES)
+  #
+#
+
+def longest(string)
+  alpha_bits = []
+  loc = 0
+  string.chars.each_with_index do |ltr, idx|
+
+    if string[idx + 1] == nil
+      alpha_bits << string[loc..idx]
+      break
+    end
+
+    if ltr.ord > string[idx + 1].ord
+      alpha_bits << string[loc..idx]
+      loc = idx + 1
+    end
+
+  end
+  max = alpha_bits.map { |e| e.size }.max
+  alpha_bits.select { |e| e.size == max }[0]
+end
+
+p longest('asd') == 'as'
+p longest('nab') == 'ab'
+p longest('abcdeapbcdef') #== 'abcde'
+p longest('asdfaaaabbbbcttavvfffffdf') == 'aaaabbbbctt'
+p longest('asdfbyfgiklag') =='fgikl'
+p longest('z') == 'z'
+p longest('zyba') == 'z'
+=end
--- other_practice_problems/odd_not_prime.rb
+++ other_practice_problems/odd_not_prime.rb
@@ -5,7 +5,55 @@
 
 # A prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
 =begin
+GOAL: return the count of numbers that are NOT prime numbers, but that ARE odd 
+between (and including) 1 and the number
+
+INPUT: positive integer
+OUTPUT: positive integer
+
+RULES
+  Explicit
+    1 is not prime
+    numbers are prime if they are only divisible by 1 and themselves
+
+  Implicit
+
+ALGORITHM
+  What kind of problem?
+    selection 
+
+  What helper methods could I use?
+    select
+    prime?
+      # return false if the number is 1
+      # create a limit variable of half num's size
+      # iterate from 2 to half numbers size
+        # return false if the number is cleanly divisible by iterator
+
+#      return true
+
+# create a collection of all numbers from 1 to given number
+# select only numbers that are odd AND not prime
+# count numbers in that collection
+
+CODE
+
 =end
+
+def prime?(num)
+  return false if num == 1
+  limit = num / 2
+  (2..limit).each do |divisor|
+    return false if num % divisor == 0
+  end
+
+  return true
+end
+
+def odd_not_prime(num)
+  (1..num).to_a.select { |n| n.odd? && !(prime?(n)) }.size
+end
+
 
 p odd_not_prime(15) == 3
 p odd_not_prime(48) == 10
