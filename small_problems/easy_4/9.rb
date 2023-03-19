30c1df8b4c3c8b45fff0b30e42558c5cbe1f7a2d
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-03-19 02:54:12 +0000

    Add solution to integer to string problem

---
+++ small_problems/easy_4/9.rb
@@ -0,0 +1,30 @@
+def integer_to_string(num)
+  digits = { 1 => '1', 2 => '2', 3 => '3',
+  4 => '4', 5 => '5', 6 => '6', 
+  7 => '7', 8 => '8', 9 => '9', 0 => '0'
+  }
+  
+  num_digits = 0
+  test = num
+  while test > 0
+    test = test / 10
+    num_digits += 1
+  end
+  num_digits = 1 if num == 0
+
+  string = ''
+  test = num
+  remainder = nil
+  num_digits.times do
+    remainder = test % 10
+    string.prepend(digits[remainder])
+    test = test / 10
+
+  end
+
+  string
+end
+
+p integer_to_string(4321) == '4321'
+p integer_to_string(0) == '0'
+p integer_to_string(5000) == '5000'
