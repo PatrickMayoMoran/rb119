bc6c111259765a78d96ba00609c162e218f53921
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-03-24 15:44:01 +0000

    Add solution to delete vowels problem

---
+++ small_problems/easy_6/delete_vowels.rb
@@ -0,0 +1,9 @@
+VOWELS = 'aeiouAEIOU'
+
+def remove_vowels(array)
+  array.map {|w| w.delete(VOWELS) }
+end
+
+p remove_vowels(%w(abcdefghijklmnopqrstuvwxyz)) == %w(bcdfghjklmnpqrstvwxyz)
+p remove_vowels(%w(green YELLOW black white)) == %w(grn YLLW blck wht)
+p remove_vowels(%w(ABC AEIOU XYZ)) == ['BC', '', 'XYZ']
