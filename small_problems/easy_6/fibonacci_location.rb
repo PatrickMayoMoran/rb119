39cecd4f659933b878b2d69f110bc35c5f1183de
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-03-25 13:10:41 +0000

    Add solution to fibonacci exercise

--- small_problems/easy_6/fibonacci_location.rb
+++ small_problems/easy_6/fibonacci_location.rb
@@ -19,15 +19,39 @@ ALGORITHM
     gen_Fibonacci -> generate the fibonacci sequence
     digits.length for number of decimal places
 
+initialize fibonacci 
+set first to 1
+set second to 1
+append first and second to fibonacci array
+while digits of the last number are less than the argument
+  new is equal to first plus second
+  new gets aded to fibonacci array
+  second is equal to first
+  first is assigned new 
+end
+
+array size
+
 CODE
 =end
 
 def find_fibonacci_index_by_length(digits)
+  sequence = []
+  first = 1
+  second = 1
+  sequence << first << second
+  while sequence.last.digits.size < digits
+    new = first + second
+    sequence << new
+    second = first
+    first = new
+  end
+  sequence.size
 end
 
-p find_fibonacci_index_by_length(2) #== 7          # 1 1 2 3 5 8 13
-p find_fibonacci_index_by_length(3) #== 12         # 1 1 2 3 5 8 13 21 34 55 89 144
-p find_fibonacci_index_by_length(10) #== 45
-# p find_fibonacci_index_by_length(100) #== 476
-# p find_fibonacci_index_by_length(1000) #== 4782
-# p find_fibonacci_index_by_length(10000) #== 47847
+p find_fibonacci_index_by_length(2) == 7          # 1 1 2 3 5 8 13
+p find_fibonacci_index_by_length(3) == 12         # 1 1 2 3 5 8 13 21 34 55 89 144
+p find_fibonacci_index_by_length(10) == 45
+p find_fibonacci_index_by_length(100) == 476
+p find_fibonacci_index_by_length(1000) == 4782
+p find_fibonacci_index_by_length(10000) == 47847
