36f3ec2bcb5d1363a648a9346e5cbde8f10a8589
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-04-26 22:15:26 +0000

    Add solution to bubble sort problem

--- small_problems/medium_2/bubble_sort.rb
+++ small_problems/medium_2/bubble_sort.rb
@@ -25,27 +25,45 @@ ALGORITHM
           set swap to true
           swap first and 2nd in array
         end
+  return array
+
+  swap helper
+    initialize 
 
 CODE
 =end
 
+def swap(array, bigger, smaller)
+  array[bigger], array[smaller] = array[smaller], array[bigger]
+end
+
 def bubble_sort!(array)
   swap_happened = true
+
   while swap_happened
     swap_happened = false
+
+    array.each_with_index do |el, i|
+      break if i > (array.size - 2)
+      after = array[i + 1]
+      if el > after
+        swap_happened = true
+        swap(array, i, i +1)
+      end
+    end
   end
 
   array
 end
 
 array = [5, 3]
-p bubble_sort!(array)
+bubble_sort!(array)
 p array == [3, 5]
 
 array = [6, 2, 7, 1, 4]
-p bubble_sort!(array)
+bubble_sort!(array)
 p array == [1, 2, 4, 6, 7]
 
 array = %w(Sue Pete Alice Tyler Rachel Kim Bonnie)
 bubble_sort!(array)
-array == %w(Alice Bonnie Kim Pete Rachel Sue Tyler)
+p array == %w(Alice Bonnie Kim Pete Rachel Sue Tyler)
